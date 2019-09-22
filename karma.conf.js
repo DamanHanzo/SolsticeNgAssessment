9f155586cfd303bffe059ac9478ca3003eb25e50
Daman Singh <singhdamanpreet1234@gmail.com>
2019-09-22 17:40:32 -0500

    Updates karma conf to facilitate tests running on hackerrank

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,31 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-mocha-reporter'),
+      require('karma-junit-reporter')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/SolsticeNgAssessment'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    angularCli: {
+      environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['mocha', 'junit'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    concurrency: Infinity,
+    junitReporter: {
+      useBrowserName: false,
+      outputFile: 'unit.xml',
+      suite: 'unit'
+    }
   });
 };
